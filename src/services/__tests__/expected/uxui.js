bc252affdba7625db82ecfcc19b83e43ba239050
rajeevnaikte <rajeevnaikte@gmail.com>
2020-05-16 19:45:59 -0400

    fix test

--- src/services/__tests__/expected/uxui.js
+++ src/services/__tests__/expected/uxui.js
@@ -1,3 +1,4 @@
-import { UX } from 'squid-ui';
-UX.add(require('/Users/rajeevn/dev/squid-uxui/src/services/__tests__/.uxui/ux/form-field-no-style.uxjs'));
-UX.add(require('/Users/rajeevn/dev/squid-uxui/src/services/__tests__/.uxui/ux/form-field-valid.uxjs'));
+import {UX} from 'squid-ui';
+
+UX.add(require('/Users/rajeevn/dev/squid-uxui/src/services/__tests__/.uxui/uxjs/form-field-no-style.uxjs'));
+UX.add(require('/Users/rajeevn/dev/squid-uxui/src/services/__tests__/.uxui/uxjs/form-field-valid.uxjs'));
